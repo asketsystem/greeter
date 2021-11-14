4f61d62c3a9a5f6cc58189a361bff6e963745747
asketsystem <asketsystem1@gmail.com>
2021-11-14 04:13:39 +0000

    Adding state variable to Greeter contract

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,9 +1,13 @@
 pragma solidity >= 0.4.0 < 0.7.0;
 
 contract Greeter {
+    string private _greeting;
+    
     function greet() external pure returns(string memory) {
         return "Hello, World!";
     }
 
-    function setGreeting(string calldata greeting) external {}
+    function setGreeting(string calldata greeting) external {
+        
+    }
 }
