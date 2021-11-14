e332321367ef700dbf80e67b34c2a998bf41d77d
asketsystem <asketsystem1@gmail.com>
2021-11-14 05:02:27 +0000

    Testing an owner exists

--- test/greeter_test.js
+++ test/greeter_test.js
@@ -1,50 +1,25 @@
 const GreeterContract = artifacts.require("Greeter");
 
 contract("Greeter", () => {
-    it("has been deployed successfully" , async () => {
-        const greeter = await GreeterContract.deployed();
-        assert(greeter, "contract was not deployed");
-    });
-});
-
-describe("greet()", () => {
-    it("returns 'Hello, World!'", async () => {
-    const greeter = await GreeterContract.deployed();
-    const expected = "Hello, World!";
-    const actual = await greeter.greet();
-    assert.equal(actual, expected, "greeted with 'Hello, World!'");
-    });
-   });
-
-contract("Greeter: update greeting", () => {
-    describe("setGreeting(string", () => {
-        it("set greeting to passed in string", async () => {
-            const greeter = await GreeterContract.deployed()
-            const expected = "Hi there!";
-
-            await greeter.setGreeting(expected);
-            const actual = await greeter.greet();
-
-            assert.equal(actual, expected, "greeting was not updated");
-        });
-    });
-});
-
-const GreeterContract = artifacts.require("Greeter");
-
-contract("Greeter", () => {
     it("has been deployed successfully", async () => {
         const greeter = await GreeterContract.deployed();
         assert(greeter, "contract failed to deploy");
     });
 
     describe("greet()", () => {
-        it("returns 'Hello, World!'", async () => {
-            const greeter = await GreeterContract.deployed();
-            const expected = "Hello, World!";
-            const actual =  await greet.greet();
-
-            assert.equal(actual, expected, "greeted with 'Hello, World!'");
+    it("returns 'Hello, World!'", async () => {
+        const greeter = await GreeterContract.deployed();
+        const expected = "Hello, World!";
+        const actual = await greeter.greet();
+        assert.equal(actual, expected, "greeted with 'Hello, World!'");
         })
     });
+    describe("owner()", () => {
+        it("returns the address of the owner", async () => {
+            const greeter = await GreeterContract.deployed();
+            const owner = await greeter.owner();
+            assert(owner, "the current owner");
+        });
+    });
 })
+   
