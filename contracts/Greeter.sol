8eef384c85106d3eb8897acfe7a6195790b3fab1
asketsystem <asketsystem1@gmail.com>
2021-11-14 04:20:14 +0000

    Updating state variable

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,12 +2,12 @@ pragma solidity >= 0.4.0 < 0.7.0;
 
 contract Greeter {
     string private _greeting;
-    
+
     function greet() external pure returns(string memory) {
         return "Hello, World!";
     }
 
     function setGreeting(string calldata greeting) external {
-        
+        _greeting = greeting;
     }
 }
