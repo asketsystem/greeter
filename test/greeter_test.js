20e26b1466a043aa9a763980175ed2021b9c228f
asketsystem <asketsystem1@gmail.com>
2021-11-14 04:40:11 +0000

    Testing an owner exists

--- test/greeter_test.js
+++ test/greeter_test.js
@@ -29,3 +29,22 @@ contract("Greeter: update greeting", () => {
         });
     });
 });
+
+const GreeterContract = artifacts.require("Greeter");
+
+contract("Greeter", () => {
+    it("has been deployed successfully", async () => {
+        const greeter = await GreeterContract.deployed();
+        assert(greeter, "contract failed to deploy");
+    });
+
+    describe("greet()", () => {
+        it("returns 'Hello, World!'", async () => {
+            const greeter = await GreeterContract.deployed();
+            const expected = "Hello, World!";
+            const actual =  await greet.greet();
+
+            assert.equal(actual, expected, "greeted with 'Hello, World!'");
+        })
+    });
+})
